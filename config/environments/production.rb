74776b8007ab0fc7d1df6cf3efbfaec9ec5ea7b3
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-08-14 13:57:04 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,12 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+# config/environments/production.rb
+
+config.logger = ActiveSupport::Logger.new('log/production.log')
+  .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+  .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -94,4 +96,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
 end
